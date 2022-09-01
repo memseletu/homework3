7b441636e095d539cb8475d13f68df60e5ddd666
Memseletu Moundae <memseletumoundae@Memseletus-MacBook-Air.local>
2022-08-31 20:17:27 -0400

    moreupdates

--- src/main/java/homework2.java
+++ src/main/java/homework2.java
@@ -1,6 +1,19 @@
 public class homework2 {
     public static void main(String[] args) {
+        //java output
         System.out.println("hello world");
-        System.out.println();
+        System.out.println( 6 * 6);
+        System.out.println("result = " + (6*6) );
+       /* String name = "Memseletu";
+        int age = 28;
+        float networth = 30000000.0f;
+        boolean alumni = true;
+        System.out.println(name +" "+ age + " "+ networth + " " + alumni);*/
+    String [] houseMembers = {"memseletu", "abdel", "jamila", "king"};
+       for(int x =0; x<houseMembers.length; x++){
+           System.out.println(houseMembers[x]);
+       }
+
+
     }
 }
